2490362fb50e7b04f62a9ef4d865816a778ba3d5
WorkingPranav <108082438+WorkingPranav@users.noreply.github.com>
2022-11-19 00:08:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'ab23d197c488
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', -1))
-SELF_DELETE = environ.get('SELF_DELETE', false)
+SELF_DELETE = environ.get('SELF_DELETE', False)
 if SELF_DELETE == "True":
     SELF_DELETE = True
 
